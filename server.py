cdb7b14e282bad12f87da4e95a0330cc3d6b88a7
o2themar <o2themar6@gmail.com>
2014-01-29 21:35:36 -0500

    Added main and handle_connection

--- server.py
+++ server.py
@@ -3,22 +3,29 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+main():
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+    s.listen(5)                 # Now wait for client connection.
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print c.recv(1000)
-    c.send('HTTP/1.0 200 OK\r\n')
-    c.send("Content-Type: text/html\r\n\r\n")
-    c.send('<html><body><h1>Hello, world</h1> this is aliomar''s Web server</body></html>')
-    c.close()
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        # Establish connection with client.    
+        c, (client_host, client_port) = s.accept()
+        print c.recv(1000)
+        handle_connection(c)
+        
+handle_connection(conn):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send("Content-Type: text/html\r\n\r\n")
+    conn.send('<html><body><h1>Hello, world</h1> this is aliomar''s Web server</body></html>')
+    conn.close()
+
+if __name__ == '__main__':
+    main()
