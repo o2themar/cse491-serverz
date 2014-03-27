a3b66579c367afb7b0780f7ce85079a0f00bf085
o2themar <aliomar@arctic.cse.msu.edu>
2014-03-27 11:34:47 -0400

    Add support for JPEG and TIFF for image app

--- imageapp/__init__.py
+++ imageapp/__init__.py
@@ -18,7 +18,7 @@ def setup():                            # stuff that should be run once.
 
 
     some_data = open('imageapp/dice.png', 'rb').read()
-    image.add_image(some_data)
+    image.add_image('imageapp/dice.png', some_data)
     
 
 def teardown():                         # stuff that should be run once.
--- imageapp/root.py
+++ imageapp/root.py
@@ -1,5 +1,7 @@
 import quixote
 from quixote.directory import Directory, export, subdir
+from quixote.util import StaticFile
+import os.path
 
 from . import html, image
 
@@ -10,9 +12,17 @@ class RootDirectory(Directory):
     def index(self):
         return html.render('index.html')
 
+    @export(name='jquery')
+    def jquery(self):
+        return html.render('jquery-1.3.2.min.js')
+
     @export(name='upload')
     def upload(self):
         return html.render('upload.html')
+
+    @export(name='upload2')
+    def upload2(self):
+        return html.render('upload2.html')
 
     @export(name='upload_receive')
     def upload_receive(self):
@@ -28,6 +38,20 @@ class RootDirectory(Directory):
         the_file.close()
         return quixote.redirect('./')
 
+    @export(name='upload2_receive')
+    def upload2_receive(self):
+        request = quixote.get_request()
+        print request.form.keys()
+
+        the_file = request.form['file']
+        print dir(the_file)
+        print 'received file with name:', the_file.base_filename
+        data = the_file.read(the_file.get_size())
+
+        image.add_image(the_file.base_filename, data)
+
+        return quixote.redirect('./')
+
     @export(name='image')
     def image(self):
         return html.render('image.html')
@@ -35,6 +59,13 @@ class RootDirectory(Directory):
     @export(name='image_raw')
     def image_raw(self):
         response = quixote.get_response()
-        response.set_content_type('image/png')
         img = image.get_latest_image()
-        return img
+        
+        if img[0].split('.')[-1].lower() in ('jpeg', 'jpg'):
+            response.set_content_type('image/jpeg')
+        elif img[0].split('.')[-1].lower in ('tif', 'tiff'):
+            response.set_content_type('image/tiff')
+        else:
+            response.set_content_type('image/png')
+        
+        return img[1]
